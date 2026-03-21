659a8d47c0ecbb9a60b4544ca246817fa60c10a1
Adam Cwikla <adamcwikla04@gmail.com>
2026-03-22 00:46:02 +0100

    Dodanie wypozyczenia i serwisu wypozyczalni

---
+++ Serwis/WypozyczalniaSerwis.cs
@@ -0,0 +1,25 @@
+﻿using Serwis.UZYTKOWNICY;
+
+namespace Serwis;
+
+public class WypozyczalniaSerwis
+{
+    private List<Uzytkownik> uzytkownicy = new List<Uzytkownik>();
+    private List<Sprzet> sprzety = new List<Sprzet>();
+    private List<Wypozyczenie> wypozyczenia = new List<Wypozyczenie>();
+
+    public void DodajUzytkownika(Uzytkownik nowyUzytkownik)
+    {
+        this.uzytkownicy.Add(nowyUzytkownik);
+    }
+
+    public void DodajSprzet(Sprzet nowySprzet)
+    {
+        this.sprzety.Add(nowySprzet);
+    }
+
+    public void DodajWypozyczenie(Wypozyczenie noweWypozyczenie)
+    {
+        this.wypozyczenia.Add(noweWypozyczenie);
+    }
+}
---
+++ Serwis/Wypozyczenie.cs
@@ -0,0 +1,23 @@
+﻿using Serwis.UZYTKOWNICY;
+
+namespace Serwis;
+
+public class Wypozyczenie
+{
+    public Wypozyczenie(Uzytkownik kto, Sprzet co, DateTime dataWypozyczenia, DateTime terminZwrotu)
+    {
+        Kto = kto;
+        Co = co;
+        DataWypozyczenia = dataWypozyczenia;
+        TerminZwrotu = terminZwrotu;
+    }
+
+    public Uzytkownik Kto { get; set; }
+    public Sprzet Co { get; set; }
+    public DateTime DataWypozyczenia { get; set; }
+    public DateTime TerminZwrotu { get; set; }
+    public DateTime? Zwrocono { get; set; }
+    
+    
+    
+}
