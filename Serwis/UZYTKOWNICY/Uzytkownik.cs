bda94e115902d85ec81987e6cef1c17ec6f15d5a
Adam Cwikla <adamcwikla04@gmail.com>
2026-03-21 22:12:42 +0100

    (zapomnialem dodac tamtym)

---
+++ Serwis/UZYTKOWNICY/Uzytkownik.cs
@@ -0,0 +1,23 @@
+﻿namespace Serwis.UZYTKOWNICY;
+
+public abstract class Uzytkownik
+{
+
+    private static int idCounter;
+
+    protected Uzytkownik(string imie, string nazwisko, int limit)
+    {
+        Id = idCounter;
+        Imie = imie;
+        Nazwisko = nazwisko;
+        Limit = limit;
+        Naliczonekary = 0;
+        idCounter++;
+    }
+
+    public int Id { get; private set; }
+    public String Imie { get; set; }
+    public String Nazwisko { get; set; }
+    public int Limit { get; set; }
+    public double Naliczonekary { get; set; }
+}
