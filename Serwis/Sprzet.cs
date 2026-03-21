69937f9927a3103fc5c1efd6b39088e2527dba70
Adam Cwikla <adamcwikla04@gmail.com>
2026-03-21 21:29:41 +0100

    Dodanie klasy abstrakcyjnej Sprzet

---
+++ Serwis/Sprzet.cs
@@ -0,0 +1,24 @@
+﻿namespace Serwis;
+
+public abstract class  Sprzet
+{
+    private static int idCounter;
+    
+
+    protected Sprzet(string nazwa, string status, double kara)
+    {
+        this.Nazwa = nazwa;
+        this.Status = status;
+        this.Kara = kara;
+        this.Id = idCounter;
+        idCounter++;
+    }
+
+    public int Id { get; private set; }
+
+    public string Nazwa { get; set; }
+
+    public string Status { get; set; }
+
+    public double Kara { get; set; }
+}
